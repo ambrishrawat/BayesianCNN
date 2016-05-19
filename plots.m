413885b050835dab402c542670283d178d8d3ec3
ambrishrawat <ambrishrawat@gmail.com>
2016-05-19 09:32:53 +0100

    img

--- plots.m
+++ plots.m
@@ -1,4 +1,4 @@
-m = csvread('stats/stats_18_5_1600.txt',1,0);
+m = csvread('stats/statsb_18_5_1600.txt',1,0);
 f = figure();
 plot(m(:,1)',m(:,2)');
 hold on
@@ -8,7 +8,7 @@ legend('cnn','bayesian cnn')
 title('error rate (original label)')
 xlabel('iterations')
 ylabel('fraction missclassification (error rate)')
-saveas(f,'stats/orig_cnn.jpg')
+saveas(f,'stats/orig_cnn_1.jpg')
 
 
 f = figure();
@@ -20,4 +20,28 @@ legend('cnn','bayesian cnn')
 title('error rate (adversarial label)')
 xlabel('iterations')
 ylabel('fraction missclassification (error rate)')
-saveas(f,'stats/adv_cnn.jpg')
+saveas(f,'stats/adv_cnn_1.jpg')
+
+
+
+f = figure();
+scatter(m(:,6)',m(:,2)');
+hold on
+scatter(m(:,6)',m(:,3)');
+hold off
+legend('cnn','bayesian cnn')
+title('error rate (original label)')
+xlabel('l2-norm')
+ylabel('fraction missclassification (error rate)')
+saveas(f,'stats/orig_cnn_rms_1.jpg')
+
+f = figure();
+scatter(m(:,6)',m(:,4)');
+hold on
+scatter(m(:,6)',m(:,5)');
+hold off
+legend('cnn','bayesian cnn')
+title('error rate (adversarial label)')
+xlabel('l2-norm')
+ylabel('fraction missclassification (error rate)')
+saveas(f,'stats/adv_cnn_rms_1.jpg')
